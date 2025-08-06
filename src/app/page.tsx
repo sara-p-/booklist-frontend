0dddef6b3aa41edd4887d9ef6225e53c997a116d
sara-p- <sarapitt88@gmail.com>
2025-08-06 17:37:33 -0400

    adding the new endpoint and making some elements to test it. Also reconfiguring the bookType type to work with the new endpoint, and throwing in some random styling to make the tests look pretty.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
 import styles from './page.module.css'
 import { BookType } from '@/types/bookType'
+import Image from 'next/image'
 
 export default async function Home() {
-  let allBooks: BookType[] = []
-  let page = 1
-  let totalPages = 1
+  const data = await fetch(
+    `https://readthatbooklist.com/wp-json/booklist/v1/books`
+  )
+  const books = await data.json()
 
-  while (page <= totalPages) {
-    const data = await fetch(
-      `https://readthatbooklist.com/wp-json/wp/v2/book?posts_per_page=100&page=${page}`
-    )
-    const books = await data.json()
-    totalPages = data.headers.get('X-WP-TotalPages')
-    allBooks = [...allBooks, ...books]
-    page++
-  }
+  const booklist = books.reverse()
 
   return (
-    <ul>
-      {allBooks.map((book: BookType) => (
-        <li key={book.id}>{book.title.rendered}</li>
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <ul className={styles.bookList}>
+        {booklist.map((book: BookType) => (
+          <li className={styles.bookItem} key={book.bookId}>
+            {/* <h2>{book.title}</h2> */}
+            <Image
+              src={book.image}
+              alt={`Book cover of ${book.title}`}
+              fill={true}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
--- src/types/bookType.ts
+++ src/types/bookType.ts
@@ -1,14 +1,62 @@
-export type BookType = {
-  id: number
-  title: {
-    rendered: string
-  }
-  slug: string
-  status: string
-  author: number[]
-  series: number[]
-  genre: number[]
-  trope: number[]
-  creature: number[]
-  booktag: number[]
+export interface BookType {
+  bookId: number
+  title: string
+  author: bookAuthor[]
+  series: bookSeries[]
+  image: string
+  genres: bookGenre[]
+  tropes: bookTrope[]
+  creatures: bookCreature[]
+  booktags: bookTag[]
+  bookNumber: string
+  publishDate: string
+  length: string
+  rating: string
+  spice: string
+  finished: boolean
+  amountCompleted: string
+  display: boolean
+  description: string
+  notes: string
+  smell: string
+  startDate: string
+  finishDate: string
+  goodreadsLink: string
+  amazonLink: string
+}
+
+type bookAuthor = {
+  authorId: number
+  authorName: string
+  authorSlug: string
+}
+
+type bookSeries = {
+  seriesId: number
+  seriesName: string
+  seriesSlug: string
+}
+
+type bookGenre = {
+  genreId: number
+  genreName: string
+  genreSlug: string
+}
+
+type bookTrope = {
+  tropeId: number
+  tropeName: string
+  tropeSlug: string
+}
+
+type bookCreature = {
+  creatureId: number
+  creatureName: string
+  creatureSlug: string
+}
+
+type bookTag = {
+  tagId: number
+  tagName: string
+  tagSlug: string
 }
