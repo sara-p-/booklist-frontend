3f1abef28c18652c73be1af48b995ea971379f03
sara-p- <sarapitt88@gmail.com>
2025-08-13 17:06:20 -0400

    working on the book item styling

--- src/components/features/Book/Book.tsx
+++ src/components/features/Book/Book.tsx
@@ -3,30 +3,45 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './Book.module.css'
 import { useFilterValuesContext } from '@/hooks/useFilterValuesContext'
+import { convertStringToDate } from '@/lib/utils'
 
 export default function Book({ book }: { book: BookType }) {
   const { filterValues } = useFilterValuesContext()
   const isList = filterValues.view === 'list'
   const itemClass = isList ? styles.bookListItem : styles.bookGridItem
+  // const publishDate = convertStringToDate(book.publishDate)
 
   return (
     <li className={itemClass} key={book.bookId}>
       <Link href={`/book/${book.slug}`} className={styles.bookLink}>
-        <Image
-          src={book.image}
-          alt={`Book cover of ${book.title}`}
-          fill={true}
-          sizes='208px, 310px'
-        />
+        <div className={styles.bookImageContainer}>
+          <Image
+            className={styles.bookImage}
+            src={book.image}
+            alt={`Book cover of ${book.title}`}
+            fill={true}
+            sizes='208px, 310px'
+          />
+        </div>
         {isList && (
           <div className={styles.bookInfo}>
             <h2 className={styles.bookTitle}>{book.title}</h2>
             <p className={styles.bookAuthor}>{book.author[0].name}</p>
-            <p className={styles.bookSeries}>{book.series[0].name}</p>
-            <p className={styles.bookPublished}>{book.publishDate}</p>
-            <p className={styles.bookLength}>{book.length} pages</p>
-            <p className={styles.bookRating}>{book.rating}</p>
-            <p className={styles.bookSpice}>{book.spice}</p>
+            <p className={styles.bookSeries}>
+              <b>series:</b> {book.series[0].name}
+            </p>
+            <p className={styles.bookPublished}>
+              <b>published:</b> {convertStringToDate(book.publishDate)}
+            </p>
+            <p className={styles.bookLength}>
+              <b>length:</b> {book.length} pages
+            </p>
+            <p className={styles.bookRating}>
+              <b>rating:</b> {book.rating}/10
+            </p>
+            <p className={styles.bookSpice}>
+              <b>spice:</b> {book.spice}/5
+            </p>
           </div>
         )}
       </Link>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,3 +6,20 @@ export function range(start, end, step = 1) {
   }
   return result
 }
+
+// Convert string to date
+export function convertStringToDate(ymdString) {
+  // Extract year, month, and day from the Ymd string
+  const year = parseInt(ymdString.substring(0, 4), 10)
+  // Month in JavaScript Date objects is 0-indexed (0 for January, 11 for December)
+  const month = parseInt(ymdString.substring(4, 6), 10) - 1
+  const day = parseInt(ymdString.substring(6, 8), 10)
+
+  // Create a new Date object
+  // Note: If you don't specify time components, they will default to 00:00:00 local time.
+  return new Date(year, month, day).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+}
