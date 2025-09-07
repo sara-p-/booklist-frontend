b663489fb05169fab277e87791e633ef0670640e
sara-p- <sarapitt88@gmail.com>
2025-09-07 14:09:35 -0400

    Feature - Mobile Close Filter Button: Add button to mobile menu panels to return to the main page

--- src/components/features/Mobile/MobileMenuCloseButton/MobileMenuCloseButton.tsx
+++ src/components/features/Mobile/MobileMenuCloseButton/MobileMenuCloseButton.tsx
@@ -1,17 +1,28 @@
 'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './MobileMenuCloseButton.module.css'
 import { useFilteredBooks } from '@/hooks/useFilteredBooks'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { DEFAULT_MOBILE_FILTER_STATE_VALUES } from '@/lib/globals'
+import useMobileFilterStateContext from '@/hooks/useMobileFilterStateContext'
 
 export default function MobileMenuCloseButton() {
   const filteredBooks = useFilteredBooks()
+  const { mobileFilterState, setMobileFilterState } =
+    useMobileFilterStateContext()
 
   if (!filteredBooks) return null
 
+  function handleCloseMobileMenu() {
+    setMobileFilterState({
+      ...mobileFilterState,
+      ...DEFAULT_MOBILE_FILTER_STATE_VALUES,
+    })
+  }
+
   return (
-    <div className={styles.container}>
-      <button className={styles.button}>
-        View {filteredBooks.length} books
-      </button>
-    </div>
+    <button className={styles.button} onClick={handleCloseMobileMenu}>
+      <FontAwesomeIcon icon={faArrowLeft} /> View {filteredBooks.length} books
+    </button>
   )
 }
