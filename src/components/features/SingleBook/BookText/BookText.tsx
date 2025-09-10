ea67d6221caee6bad9dd139d912c1050ffc2bad4
sara-p- <sarapitt88@gmail.com>
2025-09-10 15:54:07 -0400

    Style - Smell Description: Style the Smell Description differently than everything else

--- src/components/features/SingleBook/BookText/BookText.tsx
+++ src/components/features/SingleBook/BookText/BookText.tsx
@@ -14,11 +14,9 @@ export default function BookText({
   if (title === 'smell') {
     return (
       <div className={`${styles.container} ${styles.smell}`}>
-        <h3 className={styles.title}>(smell)</h3>
-        <div className={styles.textContainer}>
-          <FontAwesomeIcon icon={faQuoteLeft} className={styles.quoteLeft} />
-          <div className={styles.text}>{children}</div>
-          <FontAwesomeIcon icon={faQuoteRight} className={styles.quoteRight} />
+        <h3 className={styles.title}>smell description:</h3>
+        <div className={styles.text}>
+          <p>&quot;{children}&quot;</p>
         </div>
       </div>
     )
